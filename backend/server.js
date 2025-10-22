fa04d958863eeff7a3c32f586e3853955ade0d99
Mohammed Salah <buuza.salah99@gmail.com>
2025-10-22 03:37:52 +0200

    Cleaned up server.js.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,7 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import videoRoutes from "./routes/videoRoutes.js";
 import bodyParser from "body-parser";
-import { fileURLToPath } from "url";
-
-// Fix __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const Port = 8080;
@@ -16,9 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-// Serve converted files statically
-app.use("/converted", express.static(path.join(__dirname, "converted")));
-
+app.use("/converted", express.static("converted"));
 app.use("/api/videos", videoRoutes);
 
 app.get("/", (req, res) => {
