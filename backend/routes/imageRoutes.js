c8d7a50d670d36bcdbad74da02168b92b55be25c
Mohammed Salah <buuza.salah99@gmail.com>
2025-10-27 14:56:49 +0200

    Defined an image endpoit that user can access to convert an image.

---
+++ backend/routes/imageRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { upload } from "../config/uploadConfig.js";
+import convertImage from "../controllers/imageController.js";
+
+const router = express.Router();
+
+router.post("/convert", upload.single("file"), convertImage);
+
+export default router;
