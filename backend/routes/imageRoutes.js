aed469e535874b71ea9558046978d39ef033a7b7
Mohammed Salah <buuza.salah99@gmail.com>
2025-10-27 15:49:12 +0200

    Modified 'imageRoutes.js

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { upload } from "../config/uploadConfig.js";
-import convertImage from "../controllers/imageController.js";
+import { convertImage } from "../controllers/imageController.js";
 
 const router = express.Router();
 
